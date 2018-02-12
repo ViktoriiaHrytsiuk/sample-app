631d2d2999871feec78e5cf762ee4b1263008d19
Viktoria Hrytsiuk <grytsykvika@gmail.com>
2018-02-13 00:02:04 +0200

    Finding and authenticating a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
       # Log the user in and redirect to the user's show page.
     else
-      flash.now[:danger] = 'Invalid email/password combination'      # Create an error message.
+      # Create an error message.
       render 'new'
     end
   end
 
   def destroy
-    log_out
-    redirect_to root_url
   end
 end
