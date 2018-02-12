b9943e310e53614467fe8fef2a9198413d182487
Viktoria Hrytsiuk <grytsykvika@gmail.com>
2018-02-12 16:08:40 +0200

    Improved flash style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Create new user"
+      flash[:success] = "Welcome to the Sample App"
       redirect_to new_user_path
     else
       flash[:danger] = @user.errors.full_messages.join(", ")
