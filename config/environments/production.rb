2fedc72be2dcdccb6ddef9388c330d5da3328a1f
Viktoria Hrytsiuk <grytsykvika@gmail.com>
2018-02-18 21:09:17 +0200

    Added mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,18 +68,18 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
       :port           => '587',
-      :authentication => :plain,
+      :address        => 'smtp.sendgrid.net',
       :user_name      => ENV['SENDGRID_USERNAME'],
       :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
-      :enable_starttls_auto => true
+      :authentication => :plain
   }
+
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
