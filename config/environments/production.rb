b21b63257dd8fbd4c6e37679a01ee8821bd3659f
Viktoria Hrytsiuk <grytsykvika@gmail.com>
2018-02-18 23:07:17 +0200

    Delete sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,18 +68,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.raise_delivery_errors = true
-  host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-      :port           => '587',
-      :address        => 'smtp.sendgrid.net',
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :authentication => :plain
-  }
 
-  ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
